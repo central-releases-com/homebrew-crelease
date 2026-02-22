e6e96a5139ae89a3a1f8b3622e522f219e89b3f1
central-releases <263228219+central-releases@users.noreply.github.com>
2026-02-22 18:07:18 -0500

    Update crelease to 0.0.2

--- Formula/crelease.rb
+++ Formula/crelease.rb
@@ -1,15 +1,15 @@
 class Crelease < Formula
   desc "Central Releases CLI"
   homepage "https://central-releases.com"
-  url "https://github.com/central-releases-com/homebrew-crelease/releases/download/v0.0.1/crelease-0.0.1.tar.gz"
-  sha256 "a0243411670255a711cbc19318a74936d5ebd78113b234fe6919290411ad3696"
-  version "0.0.1"
+  url "https://github.com/central-releases-com/homebrew-crelease/releases/download/v0.0.2/crelease-0.0.2.tar.gz"
+  sha256 "287b6f3cf1c5c4ef6cf0ed4a5ba21c29c8095c819f5ae347b50f01d018054822"
+  version "0.0.2"
 
   def install
     bin.install "crelease"
   end
 
   test do
-    assert_match "0.0.1", shell_output("#{bin}/crelease --version")
+    assert_match "0.0.2", shell_output("#{bin}/crelease --version")
   end
 end
